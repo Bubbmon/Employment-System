0493314d450930d687b383c5b1a75a9c796bfac5
Bubbmon <641650804@qq.com>
2021-06-20 15:36:42 +0800

    增加talkcontroller函数 可以获得曾经聊过天的id

--- BackEnd/EmploymentSystem/src/main/java/com/system/controller/TalkController.java
+++ BackEnd/EmploymentSystem/src/main/java/com/system/controller/TalkController.java
@@ -52,8 +52,8 @@ public class TalkController {
         return "{\"dealResult\":0}";
     }
 
-    @NeedVerify
     @GetMapping("whoTalkedWithMe")
+    @NeedVerify
     public String getTalkerId(@RequestHeader String token){
         return talkService.getTalker(token);
     }
